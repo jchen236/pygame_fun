da9e56a7b128b3a4fec7e060642328b29b1ab74b
Jimmy Chen <jzc8@cornell.edu>
2017-12-18 22:00:29 -0500

    Moving and key events

--- tutorial_1/framework.py
+++ tutorial_1/framework.py
@@ -7,15 +7,25 @@ RED =  (255, 0, 0)
 
 gameDisplay = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Snake Game")
-pygame.display.update()
+lead_x = 300
+lead_y = 300
+lead_x_change = 0
 GAME_EXIT = False
+
 
 while not GAME_EXIT:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             GAME_EXIT = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                lead_x_change = -10
+            if event.key == pygame.K_RIGHT:
+                lead_x_change = 10
+    lead_x += lead_x_change
 
     gameDisplay.fill(WHITE)
+    pygame.draw.rect(gameDisplay, BLACK, [lead_x, lead_y, 10, 10])
     pygame.display.update()
         #print(event)
 
