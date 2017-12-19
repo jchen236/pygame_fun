e00d89c263b43b00d511ea40d02ca67152595d93
Jimmy Chen <jzc8@cornell.edu>
2017-12-18 21:44:41 -0500

    Events and filling

--- tutorial_1/framework.py
+++ tutorial_1/framework.py
@@ -1,7 +1,23 @@
 import pygame
 pygame.init()
 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED =  (255, 0, 0)
+
 gameDisplay = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Snake Game")
 pygame.display.update()
+GAME_EXIT = False
+
+while not GAME_EXIT:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            GAME_EXIT = True
+
+    gameDisplay.fill(WHITE)
+    pygame.display.update()
+        #print(event)
+
 pygame.quit()
 quit()
