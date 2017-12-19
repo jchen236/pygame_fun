a919d2fa6a900620d9c297588c8d418fbf86b6b8
Jimmy Chen <jzc8@cornell.edu>
2017-12-18 22:22:29 -0500

    Boundaries and y updates as well

--- tutorial_1/framework.py
+++ tutorial_1/framework.py
@@ -10,6 +10,10 @@ pygame.display.set_caption("Snake Game")
 lead_x = 300
 lead_y = 300
 lead_x_change = 0
+lead_y_change = 0
+FPS = 60
+clock = pygame.time.Clock()
+
 GAME_EXIT = False
 
 
@@ -20,13 +24,29 @@ while not GAME_EXIT:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 lead_x_change = -10
-            if event.key == pygame.K_RIGHT:
+                lead_y_change = 0
+            elif event.key == pygame.K_RIGHT:
                 lead_x_change = 10
+                lead_y_change = 0
+            elif event.key == pygame.K_UP:
+                lead_y_change = -10
+                lead_x_change = 0
+            elif event.key == pygame.K_DOWN:
+                lead_y_change = 10
+                lead_x_change = 0
+
+        # if event.type == pygame.KEYUP:
+        #     if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+        #         lead_x_change = 0
+    if lead_x >= 800 or lead_x < 0 or lead_x >= 600 or lead_y < 0:
+        GAME_EXIT = True
     lead_x += lead_x_change
+    lead_y += lead_y_change
 
     gameDisplay.fill(WHITE)
     pygame.draw.rect(gameDisplay, BLACK, [lead_x, lead_y, 10, 10])
     pygame.display.update()
+    clock.tick(FPS)
         #print(event)
 
 pygame.quit()
