67091ae66267d57ab07da30d0a05c66a607fd30b
Jimmy Chen <jzc8@cornell.edu>
2017-12-20 23:51:19 -0500

    Implement right and left movement with constraints on either side

---
+++ shmup_game/shmup.py
@@ -0,0 +1,67 @@
+import pygame
+import random
+
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Shmup")
+clock = pygame.time.Clock()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+    
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -10
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 10 
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+running = True
+while running:
+    # keep loop running at right FPS
+    clock.tick(FPS)
+    # Process input (events)
+    for event in pygame.event.get():
+        # check for window close
+        if event.type == pygame.QUIT:
+            running = False
+    
+    # Update
+    all_sprites.update()
+
+    # Draw
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+
+    # Flip the display after drawing everything
+    pygame.display.flip()
+
