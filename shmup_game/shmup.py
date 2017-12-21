ddb84f90969f483ba8fb895ee1485c9973f49e3a
Jimmy Chen <jzc8@cornell.edu>
2017-12-21 02:26:52 -0500

    Meteor rotation animation as well as random meteor generation

--- shmup_game/shmup.py
+++ shmup_game/shmup.py
@@ -53,21 +53,40 @@ class Player(pygame.sprite.Sprite):
         bullet = Bullet(self.rect.centerx, self.rect.top)
         all_sprites.add(bullet)
         bullets.add(bullet)
-    
+
+
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = meteor_img
-        self.image.set_colorkey(BLACK)
+        self.image_orig = random.choice(meteor_images)
+        self.image_orig.set_colorkey(BLACK)
+        self.image = self.image_orig.copy()
         self.rect = self.image.get_rect()
         self.radius = int(self.rect.width * 0.85 / 2)
         # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
         self.rect.x = random.randrange(WIDTH - self.rect.width)
-        self.rect.y = random.randrange(-100, -40)
+        self.rect.y = random.randrange(-150, -100)
         self.speedy = random.randrange(1, 8)
         self.speedx = random.randrange(-3, 3)
-    
+        self.rot = 0
+        self.rot_speed = random.randrange(-8, 8)
+        self.last_update = pygame.time.get_ticks()
+
+    def rotate(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > 50:
+            self.last_update = now
+            self.rot = (self.rot + self.rot_speed) % 360
+            new_image = pygame.transform.rotate(self.image_orig, self.rot)
+            old_center = self.rect.center
+            self.image = new_image
+            self.rect = self.image.get_rect()
+            self.rect.center = old_center 
+
+
+
     def update(self):
+        self.rotate()
         self.rect.y += self.speedy
         self.rect.x += self.speedx
         if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 25:
@@ -75,6 +94,7 @@ class Mob(pygame.sprite.Sprite):
             self.rect.y = random.randrange(-100, -40)
             self.speedy = random.randrange(1, 8)
             
+
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
@@ -97,6 +117,13 @@ background_rect = background.get_rect()
 player_img = pygame.image.load(os.path.join(img_dir, "ship_orange.png")).convert()
 meteor_img = pygame.image.load(os.path.join(img_dir, "meteorGrey_5.png")).convert()
 bullet_img = pygame.image.load(os.path.join(img_dir, "laserRed02.png")).convert()
+meteor_images = []
+meteor_list = ['meteorGrey_1.png', 'meteorGrey_2.png', 'meteorGrey_3.png', 
+                 'meteorGrey_4.png', 'meteorGrey_5.png', 'meteorGrey_6.png', 
+                 'meteorGrey_7.png', 'meteorGrey_8.png', 'meteorGrey_9.png', 
+                 'meteorGrey_10.png']
+for img in meteor_list:
+    meteor_images.append(pygame.image.load(os.path.join(img_dir, img)).convert())
 
 
 
