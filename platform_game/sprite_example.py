08eb211240e743f084030fab6d87669bc53e3d6c
Jimmy Chen <jzc8@cornell.edu>
2017-12-20 23:10:20 -0500

    Create player sprite class

--- platform_game/sprite_example.py
+++ platform_game/sprite_example.py
@@ -12,13 +12,23 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+class Player(pygame.sprite.Sprite):
+    # Sprite for the player
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
-
+player = Player()
+all_sprites.add(player)
 
 running = True
 while running:
