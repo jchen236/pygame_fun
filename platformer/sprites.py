eaea44e12cf47263010ed68faac598fd6f107c6f
Jimmy Chen <jzc8@cornell.edu>
2017-12-22 21:54:30 -0500

    Add slightly realistic acceleration

--- platformer/sprites.py
+++ platformer/sprites.py
@@ -1,6 +1,7 @@
 # Sprite classes for platform
 import pygame as pg
 from settings import *
+vec = pg.math.Vector2
 
 class Player(pg.sprite.Sprite):
     def __init__(self):
@@ -8,15 +9,27 @@ class Player(pg.sprite.Sprite):
         self.image = pg.Surface((30, 40))
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
-        self.vx = 0
-        self.vy = 0
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.pos = vec(WIDTH / 2, HEIGHT / 2)
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
 
     def update(self):
-        self.vx = 0
+        self.acc = vec(0, 0)
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT]:
-            self.vx = -5
+            self.acc.x = -PLAYER_ACC
         if keys[pg.K_RIGHT]:
-            self.vx = 5
-        self.rect.x += self.vx
-        self.rect.y += self.vy
+            self.acc.x = PLAYER_ACC
+        
+        # Apply friction
+        self.acc += self.vel * PLAYER_FRICTION
+        # Equations for motion
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        # wrap around side of screen
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+        self.rect.center = self.pos
