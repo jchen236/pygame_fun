4d5eb46e462907954f16cc8b742bfd1ebf8601b0
Jimmy Chen <jzc8@cornell.edu>
2017-12-22 23:43:26 -0500

    Add scoring and score rendering

--- platformer/main.py
+++ platformer/main.py
@@ -13,9 +13,11 @@ class Game:
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         self.running = True
+        self.font_name = pg.font.match_font(FONT_NAME)
     
     def new(self):
         # Start a new game
+        self.score = 0
         self.all_sprites = pg.sprite.Group()
         self.platforms = pg.sprite.Group()
         self.player = Player(self)
@@ -52,7 +54,16 @@ class Game:
                 plat.rect.y += abs(self.player.vel.y)
                 if plat.rect.top >= HEIGHT:
                     plat.kill()
+                    self.score += 1
         
+        # Die
+        if self.player.rect.bottom > HEIGHT:
+            for sprite in self.all_sprites:
+                sprite.rect.y -= max(self.player.vel.y, 10)
+                if sprite.rect.bottom < 0:
+                    sprite.kill()
+        if len(self.platforms) == 0:
+            self.playing = False 
         # Need to spawn new platforms to replenish the ones we've killed
         while len(self.platforms) < 7:
             width = random.randrange(40, 100)
@@ -73,7 +84,15 @@ class Game:
     def draw(self):
         self.screen.fill(BLACK)
         self.all_sprites.draw(self.screen)
+        self.draw_text(str(self.score), 22, WHITE, WIDTH / 2, 15)
         pg.display.flip()
+    
+    def draw_text(self, text, size, color, x, y):
+        font = pg.font.Font(self.font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x, y)
+        self.screen.blit(text_surface, text_rect)
 
     def show_start_screen(self):
         pass
